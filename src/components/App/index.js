b5d61985fcaefc324fa6a55916632364344e2f40
CoralineBouyer <coralinebbouyer@gmail.com>
2022-05-04 16:28:03 +0200

    Add style + hard-code props

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,8 @@ import './styles.scss';
 export default function App() {
   return (
     <div className="app">
-      <h1>Hello boardgame world !</h1>    
+      <h1>Hello boardgame world !</h1>
+      
     </div>
   )
 }
--- src/components/ControlledInput/index.js
+++ src/components/ControlledInput/index.js
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
-import { changeValue } from '../../actions'; // We'll need to adjust the path according to where we'll create the actions folder
+// import { useSelector, useDispatch } from 'react-redux';
+// import { changeValue } from '../../actions'; // We'll need to adjust the path according to where we'll create the actions folder
 import { Input } from 'semantic-ui-react';
+import './styles.scss';
 
 export default function ControlledInput({ name, ...otherProps }) {
-    const value = useSelector((state) => state[name]);
-    const dispatch = useDispatch();
-    const handleChange = (event) => {       
-        dispatch(changeValue(name, event.target.value));
-        // Same as:
-        // dispatch({
-        //   type: 'CHANGE_VALUE',
-        //   value: event.target.value,
-        //   key: name,
-        // });
-    };
+    // const value = useSelector((state) => state[name]);
+    // const dispatch = useDispatch();
+    // const handleChange = (event) => {       
+    //     dispatch(changeValue(name, event.target.value));
+    //     // Same as:
+    //     // dispatch({
+    //     //   type: 'CHANGE_VALUE',
+    //     //   value: event.target.value,
+    //     //   key: name,
+    //     // });
+    // };
     return (
-        <Input name={name} value={value} onChange={handleChange} {...otherProps} />
+        <Input name={'email'} value={'test@toto.com'} onChange={'handleChange'} {...otherProps} />
     );
 }
 
