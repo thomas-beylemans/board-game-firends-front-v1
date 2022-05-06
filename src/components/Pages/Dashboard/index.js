ea919f4d3e10b216ae8e1de015afccb0a7fcc71a
francois-demory <demory.f@me.com>
2022-05-06 12:38:35 +0200

    dashboard structure + footer creation

--- src/components/Pages/Dashboard/index.js
+++ src/components/Pages/Dashboard/index.js
@@ -1,9 +1,60 @@
+import { Card, Header, Grid } from 'semantic-ui-react';
+
 import Navbar from '../../../components/Navbar';
+import Banner from '../../Banner';
+import Footer from '../../Footer';
+import OneCard from '../../OneCard';
+
+import './styles.scss';
 
 export default function Dashboard() {
     return (
-        <div>
+        <div className="dashboard">
             <Navbar />
+            <Banner />
+            <Grid columns={2} stackable className="dashboard__content">
+                <Grid.Row>
+                    <Grid.Column>
+                        <Header as='h2'>Mes événements à venir</Header>
+                        <Card.Group
+                            itemsPerRow={3}
+                            stackable
+                            className="cards"
+                        >
+                            <OneCard></OneCard>
+                            <OneCard></OneCard>
+                            <OneCard></OneCard>
+                        </Card.Group>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Header as='h2'>Mes événements organisés</Header>
+                        <Card.Group
+                            itemsPerRow={3}
+                            stackable
+                            className="cards"
+                        >
+                            <OneCard></OneCard>
+                            <OneCard></OneCard>
+                            <OneCard></OneCard>
+                        </Card.Group>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Header as='h2'>Mes jeux</Header>
+                        <Card.Group
+                            itemsPerRow={3}
+                            stackable
+                            className="cards"
+                        >
+                            <OneCard></OneCard>
+                            <OneCard></OneCard>
+                            <OneCard></OneCard>
+                        </Card.Group>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            <Footer />
         </div>
     );
 }
