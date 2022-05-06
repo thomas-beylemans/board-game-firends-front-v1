01ec6b6480fbf5876d073f3c36e78a59cf222fe4
DanielPazMorales <danielfelipe12@msn.com>
2022-05-06 18:17:11 +0200

    correction of pageEvent

--- src/components/Pages/PageEvent/index.js
+++ src/components/Pages/PageEvent/index.js
@@ -1,5 +1,7 @@
 import Map from "../../Map";
 import OneCardEvent from "../../OneCardEvent";
+import Navbar from "../../Navbar";
+import Footer from "../../Footer";
 import { Header, Card } from "semantic-ui-react";
 
 // user position defined in the user profile - fetched from the database
@@ -14,8 +16,9 @@ const eventsList = [
 export default function PageEvent() {
   return (
     <>
+      <Navbar />
       <Header textAlign="center" as="h1">
-        Lorem Ipsum
+        Evénements en cours
       </Header>
       <Map
         className={"map__container--large"}
@@ -23,7 +26,7 @@ export default function PageEvent() {
         eventsList={eventsList}
       />
       <Header textAlign="center" color="orange" as="h2">
-        Lorem Ipsum
+        Mes événements
       </Header>
 
       <Card.Group itemsPerRow={3} stackable>
@@ -35,7 +38,28 @@ export default function PageEvent() {
           players="Limite de joueurs"
         />
         <OneCardEvent
-          img="https://cdn.pixabay.com/photo/2020/02/26/05/45/cards-4880676_960_720.jpg"
+          img="https://zupimages.net/up/22/18/zbcp.png"
+          title="Nom event"
+          date="Date et heure"
+          location="Lieu"
+          players="Limite de joueurs"
+        />
+        <OneCardEvent
+          img="https://zupimages.net/up/22/18/zbcp.png"
+          title="Nom event"
+          date="Date et heure"
+          location="Lieu"
+          players="Limite de joueurs"
+        />
+        <OneCardEvent
+          img="https://zupimages.net/up/22/18/zbcp.png"
+          title="Nom event"
+          date="Date et heure"
+          location="Lieu"
+          players="Limite de joueurs"
+        />
+        <OneCardEvent
+          img="https://zupimages.net/up/22/18/zbcp.png"
           title="Nom event"
           date="Date et heure"
           location="Lieu"
@@ -49,6 +73,8 @@ export default function PageEvent() {
           players="Limite de joueurs"
         />
       </Card.Group>
+
+      <Footer />
     </>
   );
 }
