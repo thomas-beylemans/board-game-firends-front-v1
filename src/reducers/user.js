fd3e9ecedbccff23d5331fb5b90604b9d9c3a354
francois-demory <demory.f@me.com>
2022-05-09 10:06:23 +0200

    delete background color

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,6 @@
 import { CHANGE_VALUE, SAVE_USER, LOGOUT, SAVE_CITY, CLEAR_ERROR, SAVE_ERROR } from "../actions/user";
 
 export const initialState = {
-<<<<<<< HEAD
     email: '',
     password: '',
     username: '',
@@ -13,20 +12,6 @@ export const initialState = {
     lat: '',
     long: '',
     isLoading: false,
-=======
-  email: '',
-  password: '',
-  passwordConfirm: '',
-  username: '',
-  accessToken: '',
-  logged: false,
-  city: '',
-  postcode: '',
-  lat: '',
-  long: '',
-  isLoading: false,
-  errorMessage: '',
->>>>>>> signin
 };
 
 const reducer = (state = initialState, action = {}) => {
