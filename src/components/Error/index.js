dbfd06e269105e7415463bfefa6b99d4d64b3ec8
CoralineBouyer <coralinebbouyer@gmail.com>
2022-05-09 19:51:19 +0200

    Error page ok but not super responsive

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -8,11 +8,12 @@ export default function Error() {
   return (
     <>
       <Image src={checkmate} className="error" centered size='big' />
-      <Segment textAlign='center' color='orange' size='large'>
+      <Segment textAlign='center' color='orange' size='large'  >
         <Header icon>
           <Icon name='frown outline' />
-          Échec et mat ! Retour à <Link to='/'>l'accueil</Link> ?
-        </Header>        
+          <p>Échec et mat !</p>
+          <p>Retour à <Link to='/'>l'accueil</Link> ?</p>
+        </Header>
       </Segment>
     </>
   );
