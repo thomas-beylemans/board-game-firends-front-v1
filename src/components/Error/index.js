c237856ebc2ba5ba26d8bd9080c74337c48724ce
CoralineBouyer <coralinebbouyer@gmail.com>
2022-05-09 19:13:56 +0200

    error page ok

---
+++ src/components/Error/index.js
@@ -0,0 +1,20 @@
+import { Segment, Image, Header, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import checkmate from '../../assets/img/checkmate.jpg';
+import './styles.scss';
+
+export default function Error() {
+
+  return (
+    <>
+      <Image src={checkmate} className="error" centered size='big' />
+      <Segment textAlign='center' color='orange' size='large'>
+        <Header icon>
+          <Icon name='frown outline' />
+          Échec et mat ! Retour à <Link to='/'>l'accueil</Link> ?
+        </Header>        
+      </Segment>
+    </>
+  );
+}
+
