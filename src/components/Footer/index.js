009b24760cec5813529024de65aeb21ea1012eee
francois-demory <demory.f@me.com>
2022-05-10 11:00:58 +0200

    modification of footer to better integrate links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,26 +1,12 @@
 import { Link } from 'react-router-dom';
-import { Grid, Header } from 'semantic-ui-react';
 import './styles.scss';
 
 export default function Footer() {
   return (
     <div className="footer">
-      <Grid columns={3}>
-        <Grid.Column className="footer__column">
-      <Grid.Row>
-        <Header as="h2" className="footer__column__title">En savoir plus...</Header>
-        </Grid.Row>
-        <Grid.Row className="footer__row">
-        <Link to="/faq" className="footer__column__link">F.A.Q</Link>
-        </Grid.Row>
-        <Grid.Row className="footer__row">
-        <Link to="/terms-of-use" className="footer__column__link">CGU</Link>
-        </Grid.Row>
-        <Grid.Row className="footer__row">
-        <Link to="/team" className="footer__column__link">Contact</Link>
-        </Grid.Row>
-        </Grid.Column>
-      </Grid>
+      <Link to="/faq" className="footer__link">F.A.Q -</Link>
+      <Link to="/terms-of-use" className="footer__link"> CGU -</Link>
+      <Link to="/team" className="footer__link"> Contact - </Link>
       Board Game Friends - © 2022
     </div>
   );
