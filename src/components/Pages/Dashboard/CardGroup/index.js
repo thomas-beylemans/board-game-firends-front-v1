0bc7d3b1a7995eb7755892adbb3b079189802887
francois-demory <demory.f@me.com>
2022-05-11 15:14:00 +0200

    add link on events cards

--- src/components/Pages/Dashboard/CardGroup/index.js
+++ src/components/Pages/Dashboard/CardGroup/index.js
@@ -4,12 +4,13 @@ import { Card, Segment, Header, Image } from 'semantic-ui-react';
 import './styles.scss';
 
 export default function CardGroup({ array, title }) {
+  console.log(array);
   return (
     <Segment className='games-segment' color='orange' padded>
       <Header as='h1' color='orange'>{title}</Header>
       <Card.Group centered children={array}>
         {array.map(card => (
-          <Card key={card.id} as={Link} to='/events'>
+          <Card key={card.id} as={Link} to={`/events/${card.id}`}>
             <Image src={card.picture} />
             <Card.Content>
               <Card.Header>{card.name}</Card.Header>
