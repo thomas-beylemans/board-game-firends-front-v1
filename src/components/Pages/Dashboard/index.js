c281f7325a603a656e2492415a96dbf01f622faa
francois-demory <demory.f@me.com>
2022-05-11 14:56:53 +0200

    fix Dashboard myEvents not refreshing

--- src/components/Pages/Dashboard/index.js
+++ src/components/Pages/Dashboard/index.js
@@ -1,8 +1,7 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { fetchAPI } from '../../../utils/fetchAPI';
 import { getUserInfos } from '../../../actions/user';
-
 import { Tab } from 'semantic-ui-react';
 
 import Navbar from '../../../components/Navbar';
@@ -18,32 +17,19 @@ export default function Dashboard() {
 
   const [loading, setLoading] = useState(false);
   const [games, setGames] = useState([]);
-  const [upcomingEvents, setUpcomingEvents] = useState([]);
+  const [allEvents, setAllEvents] = useState([]);
   const [myEvents, setMyEvents] = useState([]);
 
   const fetchUserInfos = async () => {
     setLoading(true);
-    const token = JSON.parse(localStorage.getItem('user'));
-    const userInfos = await axios.get('https://boardgamefriends.herokuapp.com/api/v1/dashboard', {
-      headers: {
-        Authorization: `Bearer ${token.accessToken}`,
-      }
-    });
-    const allEvents = userInfos.data.user.event;
-    // allEvents.filter(event => {
-    //   if (event.event_admin.username === username) {
-    //     events.push(event);
-    //   }
-    // });
-    // const events = [];
-    setMyEvents(allEvents);
-    setGames(userInfos.data.user.game);
-    setUpcomingEvents(allEvents);
+    const userInfos = await fetchAPI('dashboard');
+    setGames(userInfos.user.game);
+    setAllEvents(userInfos.user.event);
+    setMyEvents(userInfos.user.event.filter(event => event.event_admin.username === userInfos.user.username));
     setLoading(false);
   }
 
   useEffect(() => {
-    console.log('je refresh');
     dispatch(getUserInfos());
     fetchUserInfos();
   }, [dispatch]);
@@ -52,15 +38,23 @@ export default function Dashboard() {
   const tabPanels = [
     {
       menuItem: 'Mes événements à venir',
-      render: () => <Tab.Pane attached>{ loading ? <PlaceHolder array={upcomingEvents} title={'Mes événements à venir'} /> : <CardGroup array={upcomingEvents} title={'Mes événements à venir'} /> }</Tab.Pane>,
+      render: () => <Tab.Pane attached>{loading ? <PlaceHolder array={allEvents} title={'Mes événements à venir'} /> : <CardGroup array={allEvents} title={'Mes événements à venir'} />}</Tab.Pane>,
     },
     {
       menuItem: 'Mes événements organisés',
-      render: () => <Tab.Pane attached>{ loading ? <PlaceHolder array={myEvents} title={'Mes événements organisés'} /> : <CardGroup array={myEvents} title={'Mes événements organisés'} /> }</Tab.Pane>,
+      render: () => <Tab.Pane attached>
+        {
+          loading ? <PlaceHolder array={myEvents} title={'Mes événements organisés'} />
+            : <CardGroup
+              array={myEvents}
+              title={'Mes événements organisés'}
+            />
+        }
+      </Tab.Pane>,
     },
     {
       menuItem: 'Mes jeux',
-      render: () => <Tab.Pane attached>{ loading ? <PlaceHolder array={games} title={'Mes jeux'} /> : <CardGroup array={games} title={'Mes jeux'} /> }</Tab.Pane>,
+      render: () => <Tab.Pane attached>{loading ? <PlaceHolder array={games} title={'Mes jeux'} /> : <CardGroup array={games} title={'Mes jeux'} />}</Tab.Pane>,
     },
   ]
 
@@ -69,7 +63,7 @@ export default function Dashboard() {
       <Navbar />
       <Banner />
       <div className="dashboard__content">
-          <Tab panes={tabPanels} menu={{ inverted: true, attached: false, tabular: false, color: "orange", stackable: true }} />
+        <Tab panes={tabPanels} menu={{ inverted: true, attached: false, tabular: false, color: "orange", stackable: true }} />
       </div>
       <Footer />
     </div>
