7981174f32a3a72bbdccc14eb0d094b4ff7b1e0c
francois-demory <demory.f@me.com>
2022-05-11 19:17:18 +0200

    add event details on cards

--- src/components/Pages/Dashboard/CardGroup/index.js
+++ src/components/Pages/Dashboard/CardGroup/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Card, Segment, Header, Image } from 'semantic-ui-react';
+import { Card, Segment, Header, Image, Icon } from 'semantic-ui-react';
 import './styles.scss';
 
 export default function CardGroup({ array, title }) {
@@ -8,11 +8,36 @@ export default function CardGroup({ array, title }) {
     <Segment className='games-segment' color='orange' padded>
       <Header as='h1' color='orange'>{title}</Header>
       <Card.Group centered children={array} itemsPerRow={3} stackable>
-      {array.map(card => (
+        {array.map(card => (
           <Card key={card.id} as={Link} to={`/events/${card.id}`}>
             <Image src={card.picture} />
             <Card.Content>
               <Card.Header>{card.name}</Card.Header>
+            </Card.Content>
+            <Card.Content>
+              <Card.Description>
+                <Icon color="orange" name="chess queen" />
+                Organisé par {card.event_admin.username}
+              </Card.Description>
+              <Card.Description>
+                <Icon color="orange" name="clock outline" />
+                Date et heure {card.start_date}
+              </Card.Description>
+              <Card.Description>
+                <Icon
+                  color="orange"
+                  name="map marker alternate"
+                />
+                {/* Lieu {card.event.geo.city} */}
+              </Card.Description>
+              <Card.Description>
+                <Icon color="orange" name="users" />
+                Nombre de joueurs {card.seats}
+              </Card.Description>
+              <Card.Description>
+                <Icon color="orange" name="talk" />
+                Description {card.description}
+              </Card.Description>
             </Card.Content>
           </Card>
         ))}
