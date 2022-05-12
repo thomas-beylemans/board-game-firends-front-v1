914acb16141b3f105f2ef57169872411ee98ec0f
DanielPazMorales <danielfelipe12@msn.com>
2022-05-12 22:16:17 +0200

    corrected layout on modal page

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import { Button, Image, Modal, TextArea, Grid } from 'semantic-ui-react';
+import { Button, Image, Modal, TextArea, Grid, Form } from 'semantic-ui-react';
 import ControlledInput from '../ControlledInput';
 
 export default function ModalEvent() {
@@ -17,120 +17,118 @@ export default function ModalEvent() {
   return (
     <div>
       <Modal
+        as={Form}
+        onSubmit={handleSubmitCreate}
         onClose={() => setfirstModalCreateEvent(false)}
         onOpen={() => setfirstModalCreateEvent(true)}
         open={firstModalCreateEvent}
         trigger={<Button circular icon="plus circle" inverted color="yellow" />}
       >
         <Modal.Header>A propos de mon événement..</Modal.Header>
-        <form onSubmit={handleSubmitCreate}>
-          <Modal.Content image>
-            <Grid columns={2} divided stackable>
-              <Grid.Row>
-                <Grid.Column>
-                  <Image
-                    className="modal__img"
-                    fluid
-                    rounded
-                    src="https://react.semantic-ui.com/images/wireframe/image.png"
-                  />
-                </Grid.Column>
 
-                <Grid.Column>
-                  <Grid.Row>
-                    <Modal.Description>
-                      <>
-                        <Grid.Row>
-                          <ControlledInput
-                            className="modal__input"
-                            label="Nom de l'événement"
-                            name="email"
-                            type="email"
-                            placeholder="Nom de l'événement"
-                          />
-                        </Grid.Row>
+        <Modal.Content image>
+          <Grid columns={2} divided stackable>
+            <Grid.Row>
+              <Grid.Column>
+                <Image
+                  className="modal__img"
+                  fluid
+                  rounded
+                  src="https://react.semantic-ui.com/images/wireframe/image.png"
+                />
+              </Grid.Column>
 
-                        <Grid.Row>
-                          <ControlledInput
-                            className="modal__input"
-                            label="Ville"
-                            name="city"
-                            type="text"
-                            min="0"
-                            placeholder="Ville"
-                          />
-                        </Grid.Row>
-                        <Grid.Row>
-                          <ControlledInput
-                            className="modal__input"
-                            label="Code Postal"
-                            name="postcode"
-                            type="text"
-                            min="0"
-                            placeholder="Code Postal"
-                          />
-                        </Grid.Row>
-                        <Grid.Row>
-                          <ControlledInput
-                            className="modal__input"
-                            label="Nombre de joueurs"
-                            name="players"
-                            type="number"
-                            min="0"
-                            placeholder="Nombre de joueurs "
-                          />
-                        </Grid.Row>
-                        <Grid.Row>
-                          <ControlledInput
-                            className="modal__img__input"
-                            label="Date et heure"
-                            name="meeting-time"
-                            placeholder="Date et heure"
-                            type="datetime-local"
-                          />
-                        </Grid.Row>
-                        <Grid.Row>
-                          <ControlledInput
-                            className="modal__img__input"
-                            name="img"
-                            label="Image"
-                            type="file"
-                            id="file"
-                          />
-                        </Grid.Row>
-                      </>
+              <Grid.Column>
+                <Grid.Row>
+                  <Modal.Description>
+                    <>
+                      <Grid.Row>
+                        <ControlledInput
+                          className="modal__input"
+                          label="Nom de l'événement"
+                          name="email"
+                          type="email"
+                          placeholder="Nom de l'événement"
+                        />
+                      </Grid.Row>
 
-                      <TextArea
-                        className="textarea"
-                        rows={5}
-                        style={{ minWidth: 300 }}
-                        placeholder="Description"
-                        maxLength="1000"
-                      ></TextArea>
-                    </Modal.Description>
-                  </Grid.Row>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Modal.Content>
+                      <Grid.Row>
+                        <ControlledInput
+                          className="modal__input"
+                          label="Ville"
+                          name="city"
+                          type="text"
+                          min="0"
+                          placeholder="Ville"
+                        />
+                      </Grid.Row>
+                      <Grid.Row>
+                        <ControlledInput
+                          className="modal__input"
+                          label="Code Postal"
+                          name="postcode"
+                          type="text"
+                          min="0"
+                          placeholder="Code Postal"
+                        />
+                      </Grid.Row>
+                      <Grid.Row>
+                        <ControlledInput
+                          className="modal__input"
+                          label="Nombre de joueurs"
+                          name="players"
+                          type="number"
+                          min="0"
+                          placeholder="Nombre de joueurs "
+                        />
+                      </Grid.Row>
+                      <Grid.Row>
+                        <ControlledInput
+                          className="modal__img__input"
+                          label="Date et heure"
+                          name="meeting-time"
+                          placeholder="Date et heure"
+                          type="datetime-local"
+                        />
+                      </Grid.Row>
+                      <Grid.Row>
+                        <ControlledInput
+                          className="modal__img__input"
+                          name="img"
+                          label="Image"
+                          type="file"
+                          id="file"
+                        />
+                      </Grid.Row>
+                    </>
 
-          <Modal.Actions>
-            <Button
-              color="black"
-              onClick={() => setfirstModalCreateEvent(false)}
-            >
-              Retour
-            </Button>
-            <Button
-              type="submit"
-              content="Créer mon événement !"
-              labelPosition="right"
-              icon="checkmark"
-              onClick={() => setSecondModalCreateEvent(true)}
-              positive
-            />
-          </Modal.Actions>
-        </form>
+                    <TextArea
+                      className="textarea"
+                      rows={5}
+                      style={{ minWidth: 300 }}
+                      placeholder="Description"
+                      maxLength="1000"
+                    ></TextArea>
+                  </Modal.Description>
+                </Grid.Row>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Modal.Content>
+
+        <Modal.Actions>
+          <Button color="black" onClick={() => setfirstModalCreateEvent(false)}>
+            Retour
+          </Button>
+          <Button
+            type="submit"
+            content="Créer mon événement !"
+            labelPosition="right"
+            icon="checkmark"
+            onClick={() => setSecondModalCreateEvent(true)}
+            positive
+          />
+        </Modal.Actions>
 
         <Modal
           onClose={() => setSecondModalCreateEvent(false)}
