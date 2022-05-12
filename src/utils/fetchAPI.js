f01a043e4ff5cbd84fa7a9a17711a93727bdde02
francois-demory <demory.f@me.com>
2022-05-12 10:10:21 +0200

    added return of the error

--- src/utils/fetchAPI.js
+++ src/utils/fetchAPI.js
@@ -2,11 +2,17 @@ import axios from "axios";
 
 export const fetchAPI = async (route) => {
   const token = JSON.parse(localStorage.getItem('user'));
-  const result = await axios.get(`https://boardgamefriends.herokuapp.com/api/v1/${route}`, {
-    headers: {
-      Authorization: `Bearer ${token.accessToken}`,
-    }
-  });
-  return result.data;
-}
+
+  try {
+    const result = await axios.get(`https://boardgamefriends.herokuapp.com/api/v1/${route}`, {
+      headers: {
+        Authorization: `Bearer ${token.accessToken}`,
+      }
+    });
+    return result.data;
+  }
+  catch (err) {
+    return err.response.data.errorMessage;
+  }
+};
 
