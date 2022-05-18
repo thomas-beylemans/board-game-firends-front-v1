4ec246cd38e87202b4cfae53d3184cf3f313bc5a
francois-demory <demory.f@me.com>
2022-05-18 11:26:22 +0200

    add navigation to 404 if event does not exist

--- src/components/Pages/EventDetails/index.js
+++ src/components/Pages/EventDetails/index.js
@@ -2,6 +2,7 @@ import { fetchAPI } from '../../../utils/fetchAPI';
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 
 import { saveUserInfos } from '../../../actions/user';
 import { subscribeEvent, unsubscribeEvent } from '../../../actions/event';
@@ -22,12 +23,13 @@ import {
   Icon,
   Divider,
   Button,
-  Container,  
+  Container,
   Modal,
 } from 'semantic-ui-react';
 
 export default function DetailEvent() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const eventId = useParams().id;
   const loggedUser = JSON.parse(localStorage.getItem('userInfos'));
@@ -47,6 +49,11 @@ export default function DetailEvent() {
 
   const fetchEvent = async () => {
     const event = await fetchAPI(`events/${eventId}`);
+    if (event === 'Aucun événement ne correspond à la recherche !') {
+      console.log('erreur');
+      navigate("../error", { replace: true });
+    }
+    console.log(event);
     setEventTitle(event.event.name);
     setEventAdminId(event.event.event_admin.id);
     setEventAdmin(event.event.event_admin.username);
@@ -57,7 +64,7 @@ export default function DetailEvent() {
     setEvent([event.event]);
     if (userId === eventAdminId) {
       setIsAdmin(true);
-    } 
+    }
   };
 
   useEffect(() => {
@@ -77,7 +84,7 @@ export default function DetailEvent() {
   const handleSubscribeEvent = () => {
     dispatch(subscribeEvent(eventId));
   };
-  
+
   const handleUnsubscribeEvent = () => {
     dispatch(unsubscribeEvent(eventId))
   }
@@ -108,7 +115,7 @@ export default function DetailEvent() {
           title="edit avatar"
           color="orange"
           style={{ display: 'none' }}
-        >          
+        >
           <Icon name="edit" />
         </Button>
         <ControlledInput
@@ -168,76 +175,76 @@ export default function DetailEvent() {
         </Grid>
       </Segment>
 
-      { isAdmin ? (
-      <Modal
-        closeIcon
-        onClose={() => setmodalValidation(false)}
-        onOpen={() => setmodalValidation(true)}
-        open={modalValidation}
-        trigger={
-          <Button
-            onClick={handleSubscribeEvent}
-            className="eventdetail__button"
-            fluid
-            color="orange"
-            animated
-          >
-            <Button.Content visible>Participer à l'événement</Button.Content>
-            <Button.Content hidden>
-              <Icon name="calendar plus" />
-            </Button.Content>
-          </Button>
-        }
-      >
-        <Header icon="chess" content="Participation validée" />
-        <Modal.Content>
-          <p>
-            Votre participation vient d'être ajoutée à votre{' '}
-            <Link to="/dashboard"> Menu principal! </Link>
-          </p>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button color="green" onClick={() => setmodalValidation(false)}>
-            <Icon name="checkmark" /> Retour
-          </Button>
-        </Modal.Actions>
-      </Modal>
-      ) : ( 
+      {isAdmin ? (
+        <Modal
+          closeIcon
+          onClose={() => setmodalValidation(false)}
+          onOpen={() => setmodalValidation(true)}
+          open={modalValidation}
+          trigger={
+            <Button
+              onClick={handleSubscribeEvent}
+              className="eventdetail__button"
+              fluid
+              color="orange"
+              animated
+            >
+              <Button.Content visible>Participer à l'événement</Button.Content>
+              <Button.Content hidden>
+                <Icon name="calendar plus" />
+              </Button.Content>
+            </Button>
+          }
+        >
+          <Header icon="chess" content="Participation validée" />
+          <Modal.Content>
+            <p>
+              Votre participation vient d'être ajoutée à votre{' '}
+              <Link to="/dashboard"> Menu principal! </Link>
+            </p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" onClick={() => setmodalValidation(false)}>
+              <Icon name="checkmark" /> Retour
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      ) : (
 
 
-      <Modal
-        closeIcon
-        onClose={() => setmodalUnsubscribe(false)}
-        onOpen={() => setmodalUnsubscribe(true)}
-        open={modalUnsubscribe}
-        trigger={
-          <Button
-            onClick={handleUnsubscribeEvent}
-            className="eventdetail__button"
-            fluid
-            color="red"
-            animated
-          >
-            <Button.Content visible>Se désinscrire de l'événement</Button.Content>
-            <Button.Content hidden>
-              <Icon name="calendar plus" />
-            </Button.Content>
-          </Button>
-        }
-      >
-        <Header icon="chess" content="Désinscription validée" />
-        <Modal.Content>
-          <p>
-            Votre participation vient d'être supprimée de votre{' '}
-            <Link to="/dashboard"> Menu principal ! </Link>
-          </p>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button color="green" onClick={() => setmodalUnsubscribe(false)}>
-            <Icon name="checkmark" /> Retour
-          </Button>
-        </Modal.Actions>
-      </Modal>     
+        <Modal
+          closeIcon
+          onClose={() => setmodalUnsubscribe(false)}
+          onOpen={() => setmodalUnsubscribe(true)}
+          open={modalUnsubscribe}
+          trigger={
+            <Button
+              onClick={handleUnsubscribeEvent}
+              className="eventdetail__button"
+              fluid
+              color="red"
+              animated
+            >
+              <Button.Content visible>Se désinscrire de l'événement</Button.Content>
+              <Button.Content hidden>
+                <Icon name="calendar plus" />
+              </Button.Content>
+            </Button>
+          }
+        >
+          <Header icon="chess" content="Désinscription validée" />
+          <Modal.Content>
+            <p>
+              Votre participation vient d'être supprimée de votre{' '}
+              <Link to="/dashboard"> Menu principal ! </Link>
+            </p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" onClick={() => setmodalUnsubscribe(false)}>
+              <Icon name="checkmark" /> Retour
+            </Button>
+          </Modal.Actions>
+        </Modal>
       )}
 
       <Divider />
