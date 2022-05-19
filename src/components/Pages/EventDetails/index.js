1adb985dd58793429c1df5941f80c7e7a8511dd8
DanielPazMorales <danielfelipe12@msn.com>
2022-05-19 15:54:34 +0200

    alert on subscribe/unsubscribe

--- src/components/Pages/EventDetails/index.js
+++ src/components/Pages/EventDetails/index.js
@@ -76,7 +76,7 @@ export default function DetailEvent() {
       setTimeout(() => {
         dispatch(clearError());
         setIsHidden(true);
-      }, 3000);
+      }, 4000);
     }
   }, [dispatch, isSubscribed]);
 
