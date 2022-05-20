6737f6fbc5cfde29c4bcbc87cc68c65f8157d00b
DanielPazMorales <danielfelipe12@msn.com>
2022-05-20 11:05:10 +0200

    deletion in eventDetails

--- src/components/Pages/EventDetails/index.js
+++ src/components/Pages/EventDetails/index.js
@@ -154,7 +154,6 @@ export default function DetailEvent() {
                       >
                         {player.username}
                       </Link>
-                      ,{' '}
                     </span>
                   ))}
                 </Card.Description>
